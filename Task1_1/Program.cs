9e1359d451f3a5335b4b7b71b07ebcff0d34a508
kubrabm <kubrabm@code.edu.az>
2022-12-26 19:01:45 +0400

    Add project files.

---
+++ Task1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); if (input.Length < 2 || input.Length > 2)
+            {
+                Console.WriteLine("Eded iki reqemli deyil");
+                return;
+            }
+
+            int number = int.Parse(input);
+            int lastDigit = number % 10;
+            number /= 10;
+
+            Console.WriteLine($"{number} {lastDigit}");
+        }
+    }
+}
